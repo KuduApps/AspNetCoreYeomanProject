972c79ae549e018f283b08a115715cb9d239cd04
ShunXian Cai <shucai@microsoft.com>
2017-05-10 17:01:41 -0700

    init

---
+++ classLib/Class1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace classLib
+{
+    public class Class1
+    {
+		public static string returnStr(){
+			return "yeoman classlib";
+		}        
+    }
+}
